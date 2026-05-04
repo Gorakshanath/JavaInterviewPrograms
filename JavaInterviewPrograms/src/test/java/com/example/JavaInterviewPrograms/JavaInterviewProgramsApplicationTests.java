db3b9b5e212829449117cb0c5c2bbdb83c4a5457
guru <gggurughule3@gmail.com>
2026-05-04 17:20:39 +0530

    first commit

---
+++ JavaInterviewPrograms/src/test/java/com/example/JavaInterviewPrograms/JavaInterviewProgramsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JavaInterviewPrograms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInterviewProgramsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
