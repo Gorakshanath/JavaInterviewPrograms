78254784eb5ae40aa6ea29a07b672e41dff621b8
Gorakshanath <gggurughule3@gmail.com>
2026-06-26 14:44:43 +0530

    Added firstDuplicateCharacterFromWord method

--- JavaInterviewPrograms/src/main/java/com/example/JavaInterviewPrograms/InterviewAskedPrograms.java
+++ JavaInterviewPrograms/src/main/java/com/example/JavaInterviewPrograms/InterviewAskedPrograms.java
@@ -1,18 +1,17 @@
 package com.example.JavaInterviewPrograms;
 
-import java.util.Arrays;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class InterviewAskedPrograms {
     public static void main(String[] args){
-        //First non-repeating character — return the first character
+        //First non-repeating
+        // character — return the first character
         // that appears only once. "swiss" → 'w'
-        firstNonRepeatingCharacter("hdfhfxaswiss");
+        firstNonRepeatingCharacterFromWord("hdfhfxaswiss");
+        firstDuplicateCharacterFromWord("swiss");
     }
-    private static void firstNonRepeatingCharacter(String word){
+    private static void firstNonRepeatingCharacterFromWord(String word){
         String[] array=word.split("");
         Optional<String> duplicateCharacter=Arrays.asList(array).stream()
                 .collect(Collectors.groupingBy(c->c, LinkedHashMap::new,Collectors.counting()))
@@ -21,5 +20,17 @@ public class InterviewAskedPrograms {
         if(duplicateCharacter.isPresent()) {
             System.out.println(duplicateCharacter.get());
         }
+    }
+    private static void firstDuplicateCharacterFromWord(String word){
+        String[] array=word.split("");
+        Set<String> set=new HashSet<>();
+//        for(String c:array){
+//            if(!set.add(c)){
+//                System.out.println("First Duplicate Character :"+c);
+//                break;
+//            }
+//        }
+        // Java8
+        Arrays.asList(array).stream().filter(c->!set.add(c)).forEach((System.out::println));
     }
 }
